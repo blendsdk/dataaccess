c0103605940f455ed19a8a434b34dc497cbc12ce
Gevik Babakhani <gevik@blendjs.com>
2019-01-07 16:34:08 +0100

    Added a simple user roles model

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,3 +2,4 @@ export * from "./database/Database";
 export * from "./postgresql/PostgreSQLDatabase";
 export * from "./typescript/FactoryBuilder";
 export * from "./typescript/TypeBuilder";
+export * from "./models/users_and_roles";
---
+++ packages/schema/src/models/users_and_roles.ts
@@ -0,0 +1,84 @@
+import { Database } from "../database/Database";
+import { Table } from "../database/Table";
+import { eQueryMethod, FactoryBuilder } from "../typescript/FactoryBuilder";
+
+/**
+ * Interface describing the return value of `createUsersAndRoles()`
+ *
+ * @export
+ * @interface ICreateUsersAndRoles
+ */
+export interface ICreateUsersAndRoles {
+    sys_user: Table;
+    sys_role: Table;
+}
+
+/**
+ * Creates a generic user and role table combination
+ *
+ * @export
+ * @param {Database} db
+ * @param {FactoryBuilder} fb
+ * @returns {ICreateUsersAndRoles}
+ */
+export function createUsersAndRoles(db: Database, fb: FactoryBuilder): ICreateUsersAndRoles {
+    const sys_user = db.addTable("sys_user"),
+        sys_role = db.addTable("sys_role"),
+        sys_user_role = db.addTable("sys_user_role");
+
+    sys_user
+        .primaryKeyColumn()
+        .stringColumn("username", { unique: true })
+        .stringColumn("password")
+        .stringColumn("email", { unique: true })
+        .dateTimeColumn("date_created", { default: "now()" })
+        .booleanColumn("is_active", { default: "true" });
+
+    sys_user_role
+        .referenceColumn("user_id", sys_user)
+        .referenceColumn("role_id", sys_role)
+        .uniqueConstraint(["user_id", "role_id"]);
+
+    sys_role
+        .primaryKeyColumn()
+        .stringColumn("role", { unique: true })
+        .stringColumn("description", { required: false });
+
+    fb.addMethod([
+        {
+            forTable: sys_user,
+            methodName: "findUser",
+            parameters: [
+                {
+                    name: "username",
+                    type: "string"
+                }
+            ],
+            queryMethod: eQueryMethod.executeQuerySingle,
+            query: "SELECT * FROM sys_user WHERE lower(username)=lower(:username) or lower(email)=lower(:username)"
+        },
+        {
+            forTable: sys_user,
+            methodName: "getUserRoles",
+            returnType: "ISysUserRole",
+            returnTypeIsArray: true,
+            description: "Given a user_id this method returns the user roles",
+            parameters: {
+                name: "user_id",
+                type: "number"
+            },
+            queryMethod: eQueryMethod.executeQuerySingle,
+            query: `
+					select
+						r.*
+					from
+						sys_user_role ur
+						inner join sys_user u on u.id = ur.user_id
+						inner join sys_role r on r.id = ur.role_id
+					where
+						u.id = :user_id
+					`
+        }
+    ]);
+    return { sys_user, sys_role };
+}
